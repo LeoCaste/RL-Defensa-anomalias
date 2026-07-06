bb0b4f23057df1f296f9abdb33c3a72c92b68239
Leo Castellon <l.castellon01@ufromail.cl>
2026-07-05 21:54:18 -0400

    feat(backend): configurar base de Spring Boot

---
+++ backend/src/main/java/com/rlanomaly/backend/RlAnomalyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.rlanomaly.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlAnomalyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RlAnomalyBackendApplication.class, args);
+    }
+}
